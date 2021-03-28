620ca181e188880ee53e58ef24c1d2c7f2f8f7b3
VanillaScent <Vanillascent@github.com>
2021-03-28 03:53:26 +0200

    Proper documentation and testcases added again.

--- README.md
+++ README.md
@@ -9,3 +9,19 @@ the main page would consist of mostly modals where it would be webstream of live
 A team area where there will be a live chat for the specific created teams and a referral system
 
 a purchase/cart page where payments would be acceptep in crypto
+
+### Getting started
+
+
+Create virtual env:
+`python3 -m venv env`
+Activate:
+`source ./env/bin/activate`
+
+Requirements:
+
+- Django (3.1.7)
+- django-rest-swagger (2.2.0)
+- djangorestframework (3.12.2)
+- drf-yasg (1.20.0)
+- python-binance (0.7.9)
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,50 @@
-from django.test import TestCase
+import unittest
+import json
 
-# Create your tests here.
+from random import randint
+from django.test import Client
+
+from .models import User
+
+
+class UserTestCase(unittest.TestCase):
+    def setUp(self):
+        random = randint(0, 99)
+        
+        self.data = {
+            'username': 'testCase{0}'.format(random),
+            'email': 'testCase{0}@server.local'.format(random),
+            'password': 'testCase{0}'.format(random),
+        }
+        self.headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        }
+
+        #http client
+        self.client = Client()
+
+
+    def test_register(self):
+        """Test POST /api/users/ creation"""
+
+        response = self.client.post('/api/users/', 
+                data=self.data, 
+                headers=self.headers
+        )
+
+        self.assertEqual(response.status_code, 201)
+    
+    def test_login(self):
+        """Test POST /login"""
+
+        response = self.client.post('/login', 
+                data={
+                    'username': self.data['username'],
+                    'password': self.data['password'],
+                }, 
+                headers=self.headers
+        )
+        
+        self.assertIn(response.content.decode('utf-8'), 'href="/accounts/profile"')
+
